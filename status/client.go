33dde933c1a2d80157084d8949c5fcd20cad8f71
Dennis Pietruck <Dennis.Pietruck@haw-hamburg.de>
2019-10-18 17:21:08 +0200

    Complete client

--- status/client.go
+++ status/client.go
@@ -3,23 +3,43 @@ package status
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"net/http"
 )
+
 // Remove our server from a subscription list of a remote server
 func RemoveSubscription(profile Profile, server string) error {
 	b := new(bytes.Buffer)
 	json.NewEncoder(b).Encode(profile)
-	res, _ := http.NewRequest(http.MethodDelete, server + "/subscribers", b)
+	res, _ := http.NewRequest(http.MethodDelete, server+"/subscribers", b)
+	if res.Response.StatusCode > 200 {
+		return fmt.Errorf("Received status %s from %s", res.Response.Status, server)
+	}
+	return nil
 }
 
 // Add our server to a subscription list of a remote server
 func AddSubscription(profile Profile, server string) error {
-	postRequest(profile, server + "/subscribers")
+	res := postRequest(profile, server+"/subscribers")
+	if res.StatusCode > 200 {
+		return fmt.Errorf("received status %s from %s", res.Status, server)
+	}
+	return nil
 }
 
-
-func Notify(status ProfileStatus) error {
-	panic("implement me")
+func Notify(status ProfileStatus, subscribers map[string]Profile) error {
+	errors := make([]string, 0)
+	for _, subscriber := range subscribers {
+		res := postRequest(status, subscriber.URL+"/subscribers")
+		if res.StatusCode > 200 {
+			message := fmt.Sprintf("received status %s from %s", res.Status, subscriber.URL)
+			errors = append(errors, message)
+		}
+	}
+	if len(errors) > 0 {
+		return fmt.Errorf("errors during notify %v", errors)
+	}
+	return nil
 }
 
 func postRequest(obj interface{}, url string) *http.Response {
